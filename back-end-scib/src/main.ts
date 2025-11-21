fbbf5adecca0d7fed4c75886dc296d043079c2ee
Carolina Ros <rosdelreal@gmail.com>
2025-11-21 19:33:03 +0100

    chore(backend): update package files and main configuration

--- back-end-scib/src/main.ts
+++ back-end-scib/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('SCIB Candidates API')
+    .setDescription('Endpoints for loading candidates')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(port, '0.0.0.0');
 
